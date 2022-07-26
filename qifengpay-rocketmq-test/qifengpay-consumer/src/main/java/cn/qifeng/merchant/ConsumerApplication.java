25f65256ae5a5b12f45a71c66bdecab3c4cabcb5
fruit9659 <92135067+fruit9659@users.noreply.github.com>
2022-07-27 03:24:04 +0800

    Add files via upload

---
+++ qifengpay-rocketmq-test/qifengpay-consumer/src/main/java/cn/qifeng/merchant/ConsumerApplication.java
@@ -0,0 +1,17 @@
+package cn.qifeng.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
